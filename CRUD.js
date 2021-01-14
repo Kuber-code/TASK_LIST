9a03971940dd934977f46058ac375b14d7c59ef4
Kuber-code <kuba.lichosik@gmail.com>
2021-01-14 17:48:01 +0100

    CRUD.js file edited
    many changes in CRUD.js file

--- CRUD.js
+++ CRUD.js
@@ -1,21 +1,18 @@
-//przekazywanie textu do htmla
-//document.getElementById("text").innerHTML = "TEKST Z PLIKU JS";
-
 //czyszenie localstorage
 //localStorage.clear();
-//document.addEventListener(scriptLoad(), function() {});
-document.addEventListener("DOMContentLoaded", function() {
-  var storage = (function(key) {
+
+document.addEventListener('DOMContentLoaded', function() {
+  const storage = (function(key) {
     if (key === undefined) {
-      key = "localList";
+      key = 'localList';
     }
-    var saveIternal = function(toSave) {
+    const saveLocal = function(toSave) {
       localStorage.setItem(key, JSON.stringify(toSave));
     };
     return {
-      save: saveIternal,
+      save: saveLocal,
       loadOrElse: function(defaultValue) {
-        var loadedValue = localStorage.getItem(key);
+        const loadedValue = localStorage.getItem(key);
         if (loadedValue) {
           return JSON.parse(localStorage.getItem(key));
         } else {
@@ -28,42 +25,50 @@ document.addEventListener("DOMContentLoaded", function() {
     };
   })();
 
-  var TaskListApp = (function(storage) {
-    var defaultList = ["10", "20", "30"];
-    var taskList = storage.loadOrElse(defaultList);
+  const TaskListApp = (function(storage) {
+    const defaultList = ['10', '20', '30'];
+    let taskList = storage.loadOrElse(defaultList);
 
     function viewTasks() {
-      let list = document.getElementById("list");
+      const list = document.getElementById('list');
       while (list.hasChildNodes()) {
         list.removeChild(list.firstChild);
+      }
+
+      function addingRemoveButton(){
+        const removeButton = document.createElement('input');
+        removeButton.type = 'button';
+        removeButton.value = 'Delete this';
+        removeButton.addEventListener('click', () => TaskListApp.remove(i));
+        listElement.appendChild(removeButton);
       }
 
       for (let i in taskList) {
         const task = taskList[i];
 
-        let listElement = document.createElement("li");
-        let taskmsg = document.createTextNode(task);
+        const listElement = document.createElement('li');
+        const taskmsg = document.createTextNode(task);
         listElement.appendChild(taskmsg);
-
-        let removeButton = document.createElement("input");
-        removeButton.type = "button";
-        removeButton.value = "Delete this";
-        removeButton.addEventListener("click", () => TaskListApp.remove(i));
+      
+        const removeButton = document.createElement('input');
+        removeButton.type = 'button';
+        removeButton.value = 'Delete this';
+        removeButton.addEventListener('click', () => TaskListApp.remove(i));
         listElement.appendChild(removeButton);
 
-        let editButton = document.createElement("button");
-        editButton.addEventListener("click", function() {
+        const editButton = document.createElement('button');
+        editButton.addEventListener('click', function() {
           TaskListApp.edit(i);
         });
-        editButton.innerHTML = "Edit task with text from box";
+        editButton.innerHTML = 'Edit task with text from box';
         listElement.appendChild(editButton);
 
         // html += `<li>${task}
-        //  <button onclick='TaskListApp.remove("${i}")'>Delete this</button>
-        //  <button onclick='TaskListApp.edit("${i}")'>Edit task with text from box</button>
+        //  <button onclick='TaskListApp.remove('${i}')'>Delete this</button>
+        //  <button onclick='TaskListApp.edit('${i}')'>Edit task with text from box</button>
         //  </li>`;
-        // to zakomentowane i to poniżej działa tak samo
-        //html += "<li>" + task + "<button onclick='removeTask(" + i + ")'>Delete this</button><button onclick='editTask(" + i + ")'>Edit task with text from box</button></li>";
+        // to na górze i to poniżej działa tak samo
+        //html += '<li>' + task + '<button onclick='removeTask(' + i + ')'>Delete this</button><button onclick='editTask(' + i + ')'>Edit task with text from box</button></li>';
         list.appendChild(listElement);
       }
 
@@ -72,25 +77,29 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function removeTask(indexToBeRemoved) {
       taskList.splice(indexToBeRemoved, 1);
-      //console.error('Nie usuwaj mnie!');
-      //console.warn(':((((');
       viewTasks();
     }
 
     function addTask() {
-      let userInput = document.getElementById("msg").value;
-      document.getElementById("msg").value = ""; // czyszczenie inputa
+      const userInput = document.getElementById('msg').value;
+      inputCleaner();
       taskList.push(userInput);
       viewTasks();
     }
 
-    var editTask = function(indexToBeRemoved) {
+    function inputCleaner() {
+      //const inputValue = document.getElementById('msg').value;
+      //inputValue = ' ';
+      document.getElementById('msg').value = ''; // czyszczenie inputa
+    }
+
+    const editTask = function(indexToBeRemoved) {
       taskList.splice(
         indexToBeRemoved,
         1,
-        document.getElementById("msg").value
+        document.getElementById('msg').value
       );
-      document.getElementById("msg").value = ""; // czyszczenie inputa
+      inputCleaner();
       viewTasks();
     };
 
@@ -114,11 +123,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
   TaskListApp.view();
 
-  document
-    .getElementById("addTaskButton")
-    .addEventListener("click", TaskListApp.add);
+  document.getElementById('addTaskButton').addEventListener('click', TaskListApp.add); //to samo
 
-  document.getElementById("clearButton").addEventListener("click", () => {
-    TaskListApp.clear(); //to samo
+  document.getElementById('clearButton').addEventListener('click', () => {TaskListApp.clear(); //to samo
   });
 });
